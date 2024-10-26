4ec5562948a29fe9401dc432974e96ec6ac44224
ekatsevi <katsevich.gene@gmail.com>
2024-10-26 12:28:13 -0400

    Initial commit

---
+++ R/launch_app.R
@@ -0,0 +1,11 @@
+#' Launch the PerturbPlan Shiny App
+#'
+#' @return None
+#' @export
+launch_app <- function() {
+  app_dir <- system.file("shiny", package = "perturbplan")
+  if (app_dir == "") {
+    stop("Could not find app directory. Try reinstalling the `perturbplan` package.", call. = FALSE)
+  }
+  shiny::runApp(app_dir)
+}
