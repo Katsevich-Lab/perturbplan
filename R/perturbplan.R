bf80a8f5411d3c8d7325c7aafc4d449b6cf6f352
ekatsevi <katsevich.gene@gmail.com>
2025-01-21 20:28:45 -0500

    Added global variables to perturbplan.R

---
+++ R/perturbplan.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("expression_mean", "expression_size", "gRNA_target",
+                         "grna_id", "grna_target",
+                         "num_cells", "pair", "response_id"))
