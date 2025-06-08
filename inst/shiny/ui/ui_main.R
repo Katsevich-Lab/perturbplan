2a62bc3a2c4417f2157cff3217c2d0dc492ae3bf
Ziang Niu <ziangniu6@gmail.com>
2025-06-08 10:03:23 -0400

    Revert "Fix scrolling issue with split-panel layout"
    This reverts commit f58b4920d007e74187872a697636f1e3796f1267.

--- inst/shiny/ui/ui_main.R
+++ inst/shiny/ui/ui_main.R
@@ -14,9 +14,9 @@ ui <- dashboardPage(
       menuItem("Power over TPM & FC", tabName = "per_pair", icon = icon("project-diagram"))
     ),
     
-    # Parameter panels - make scrollable while keeping right panel fixed
+    # Parameter panels
     tags$div(
-      style = "padding: 15px; height: calc(100vh - 100px); overflow-y: auto; position: fixed; width: 230px;",
+      style = "padding: 15px;",
       
       # Experimental choices
       h4("Experimental choices"),
@@ -56,28 +56,9 @@ ui <- dashboardPage(
     )
   ),
   
-  # Dashboard body - fixed position, no scrolling
+  # Dashboard body
   dashboardBody(
     useShinyjs(),
-    
-    # Custom CSS to ensure right panel is fixed
-    tags$head(
-      tags$style(HTML("
-        .content-wrapper {
-          position: fixed;
-          top: 50px;
-          left: 230px;
-          right: 0;
-          bottom: 0;
-          overflow: hidden;
-        }
-        .content {
-          height: 100%;
-          overflow-y: auto;
-          padding: 15px;
-        }
-      "))
-    ),
     
     # Tab content
     tabItems(
