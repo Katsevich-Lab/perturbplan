03664e8ecce99d0002185649cb65085570eaa957
Ziang Niu <ziangniu6@gmail.com>
2025-06-10 17:23:58 -0400

    Remove tracked build artifacts directory from repository

--- perturbplan.Rcheck/00_pkg_src/perturbplan/src/var_nb.h
+++
@@ -1,9 +0,0 @@
-#ifndef VAR_NB_H            // include-guard
-#define VAR_NB_H
-
-/* Negative-binomial variance: Var(Y) = μ + μ² / size                    */
-inline double var_nb(double mean, double size) {
-  return mean + (mean * mean) / size;
-}
-
-#endif
