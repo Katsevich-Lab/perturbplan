77c452ec2bcc38fb3220ccf1ce6d530f51d6c5ec
yihuihe4Wharton <yihuihe@upenn.edu>
2025-06-15 17:51:45 +0800

    fix bug

--- R/pilot_data_preprocessing.R
+++ R/pilot_data_preprocessing.R
@@ -78,7 +78,7 @@ obtain_expression_information <- function(response_matrix,
       g_idx <- gene_row_idx[i]
       y     <- as.numeric(response_matrix[g_idx, , drop = FALSE])
       mu    <- library_size * rel_expr[g_idx]
-      th    <- obtain_dispersion(y, mu, rough = rough)
+      th    <- compute_theta_cpp(y, mu, rough = rough)
       max(min(th, 1e3), 0.01)   # clip
     },
     future.seed = TRUE
