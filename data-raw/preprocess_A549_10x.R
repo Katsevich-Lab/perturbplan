2610708ea64e3cc4cf7a83c6587ec7b0b0ed6dca
Ziang Niu <ziangniu6@gmail.com>
2025-07-21 14:26:09 -0400

    ready to preprocess the data

---
+++ data-raw/preprocess_A549_10x.R
@@ -0,0 +1,15 @@
+# load path to raw data directory
+devtools::load_all()
+source("~/.Rprofile")
+path_to_raw_data <- paste0(.get_config_path("LOCAL_SAKELLAROPOULOS_2024_RAW_DATA_DIR"), "/processed")
+
+# extract response matrix and read-UMI table
+preprocessed_data <- reference_data_preprocessing_10x(path_to_raw_data)
+
+# obtain the necessary information for baseline expression and library size information
+preprocessed_results <- reference_data_preprocessing(response_matrix = response_matrix,
+                                                     read_umi_table = read_umi_table)
+
+# save the baseline information to data
+saveRDS(preprocessed_results, "data/A549_10x.rds")
+
---
+++ data-raw/preprocess_iPSC_10x.R
@@ -0,0 +1,14 @@
+# load path to raw data directory
+devtools::load_all()
+source("~/.Rprofile")
+path_to_raw_data <- paste0(.get_config_path("LOCAL_TIAN_2019_RAW_DATA_DIR"), "/processed")
+
+# extract response matrix and read-UMI table
+preprocessed_data <- reference_data_preprocessing_10x(path_to_raw_data)
+
+# obtain the necessary information for baseline expression and library size information
+preprocessed_results <- reference_data_preprocessing(response_matrix = response_matrix,
+                                                     read_umi_table = read_umi_table)
+
+# save the baseline information to data
+saveRDS(preprocessed_results, "data/iPSC_10x.rds")
