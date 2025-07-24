3265888230bddc4a2ead78e28dc2eb65243a462d
Ziang Niu <ziangniu6@gmail.com>
2025-07-24 13:14:29 -0400

    update the input parameter for preprocessing

--- data-raw/preprocess_iPSC_neuron_10x.R
+++ data-raw/preprocess_iPSC_neuron_10x.R
@@ -8,7 +8,9 @@ preprocessed_data <- reference_data_preprocessing_10x(path_to_raw_data)
 
 # obtain the necessary information for baseline expression and library size information
 preprocessed_results <- reference_data_preprocessing(response_matrix = preprocessed_data$response_matrix,
-                                                     read_umi_table = preprocessed_data$read_umi_table)
+                                                     read_umi_table = preprocessed_data$read_umi_table,
+                                                     D2_rough = 0.1,
+                                                     downsample_ratio = c(0.1, 0.3, 0.5, 0.7))
 
 # save the baseline information to data
 save(preprocessed_results, "data/iPSC_neuron_10x.rda")
