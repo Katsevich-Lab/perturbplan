b4bbbe1de6228c2b6bd7425e830aa0b792c24fbd
ekatsevi <katsevich.gene@gmail.com>
2025-09-01 18:25:08 -0400

    Fixed check

--- inst/data-raw/preprocess_A549_10x.R
+++ inst/data-raw/preprocess_A549_10x.R
@@ -1,14 +1,15 @@
 # load path to raw data directory
 devtools::load_all()
 source("~/.Rprofile")
-path_to_raw_data <- paste0(.get_config_path("LOCAL_TIAN_2019_RAW_DATA_DIR"), "/processed/iPSC")
+path_to_raw_data <- paste0(.get_config_path("LOCAL_SAKELLAROPOULOS_2024_RAW_DATA_DIR"), "/processed")
 
 # extract response matrix and read-UMI table
 preprocessed_data <- reference_data_preprocessing_10x(path_to_raw_data)
 
 # obtain the necessary information for baseline expression and library size information
-iPSC_10x <- reference_data_preprocessing(response_matrix = preprocessed_data$response_matrix,
+A549_10x <- reference_data_preprocessing(response_matrix = preprocessed_data$response_matrix,
                                          read_umi_table = preprocessed_data$read_umi_table)
 
 # save the baseline information to data
-usethis::use_data(iPSC_10x, overwrite = TRUE)
+usethis::use_data(A549_10x, overwrite = TRUE, compress = "xz")          # allow reruns
+
